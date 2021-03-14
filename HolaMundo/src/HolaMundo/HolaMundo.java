43bc5d2b92888700d9f74521118c8e1e83910085
Adri <Adri@Adri-PC>
2021-03-14 13:23:46 +0100

    Primer Commit Eclipse

---
+++ HolaMundo/src/HolaMundo/HolaMundo.java
@@ -0,0 +1,10 @@
+package HolaMundo;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	System.out.println("Hola Adri 14-03-2021 13.17");
+    }
+
+}
